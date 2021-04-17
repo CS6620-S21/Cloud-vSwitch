7923c93adca568d741a42aa7cb831123facd8c17
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-04-17 02:05:42 -0400

    Fix easy-rsa in install script

--- API/scripts/install.sh
+++ API/scripts/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Script to install server dependencies on CentOS
 sudo yum -y install openssl
-sudo yum -y install easy-rsa
+sudo yum -y install easy-rsa-3.0.8-1.el7
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
 sudo yum -y install nodejs && npm install pm2@latest -g
 # Copy easy-rsa to home directory
-cp -r /usr/share/easy-rsa/3.0.8 "$HOME/easy-rsa"
+mkdir "$HOME"/easy-rsa
+sudo cp -r /usr/share/easy-rsa/3.0.8/* "$HOME"/easy-rsa/
