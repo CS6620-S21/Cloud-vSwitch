b8ec270df9ad91c99d8ae5c5127acc80f4630616
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-04-17 02:31:13 -0400

    Download easy-rsa from GitHub

--- API/scripts/install.sh
+++ API/scripts/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Script to install server dependencies on CentOS
 sudo yum -y install openssl
-sudo yum -y install easy-rsa-3.0.8-1.el7
+
+mkdir "$HOME"/easy-rsa
+curl -L https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz | tar -xz -C "$HOME"/easy-rsa --strip-components=1
+
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
 sudo yum -y install nodejs && npm install pm2@latest -g
-# Copy easy-rsa to home directory
-mkdir "$HOME"/easy-rsa
-sudo cp -r /usr/share/easy-rsa/3.0.8/* "$HOME"/easy-rsa/
